cd67b4f7021a273db490d4585e23d644218d72b8
Shreshtha Gupta <101451400+Shreshtha-1234@users.noreply.github.com>
2023-08-02 13:13:51 +0000

    data type conversions

---
+++ 01_basics/04_dataTypeConversion.js
@@ -0,0 +1,24 @@
+// convert to number
+
+let a = Number("Rahul32")
+let b = Number(false)
+let c = Number(null)
+let d = Number(undefined)
+
+console.table([a,b,c,d]);
+
+// convert to boolean
+let e = Boolean("")
+let f = Boolean("RAHUL")
+let g = Boolean(-7653)
+let h = Boolean(0)
+
+console.table([e,f,g,h]);
+
+//convert to string
+let i = String(23)
+let j = String(true)
+let k = String(undefined)
+let l = String(null)
+
+console.table([i,j,k,l]);
