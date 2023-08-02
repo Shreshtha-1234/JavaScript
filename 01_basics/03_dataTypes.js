3b98ed3fe1054cfcd4e409acf64c353b8596fd68
Shreshtha Gupta <101451400+Shreshtha-1234@users.noreply.github.com>
2023-08-02 09:16:32 +0000

    data types

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,13 @@
+// This is basic introduction of data types in js
+
+let a = "Shrey"
+let b = 1835
+let c = true
+let d = null
+let e = undefined
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
