651c423777c022b417b71456dcb1cd9b288f8416
Shreshtha Gupta <101451400+Shreshtha-1234@users.noreply.github.com>
2023-08-02 13:29:25 +0000

    important

---
+++ 01_basics/05_operationsStringNumber.js
@@ -0,0 +1,22 @@
+
+console.log("hello"+" world")
+
+console.log(1 + 2 + "2")
+
+console.log("1" + 2 + 2)
+
+console.log(true)
+
+console.log(+true)
+
+console.log(-true)
+
+console.log(+null)
+
+console.log(+undefined)
+
+console.log(+"")
+
+let a , b ,c;
+a=b=c=5*9
+console.log(b)
