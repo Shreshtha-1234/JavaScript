f2995f9e34bedaef57769c5d066db4fb4f51f551
Shreshtha Gupta <101451400+Shreshtha-1234@users.noreply.github.com>
2023-08-04 14:49:38 +0000

    function

--- 03_ExploringJS/01_functions.js
+++ 03_ExploringJS/01_functions.js
@@ -1,8 +1,50 @@
+// functions can have default value in parameter
 
-function add(number1 , number2)
-{
-    return number1 + number2
-}
+// -> 1 . Defining function
+// function add(number1 , number2=90)
+// {
+//     return number1 + number2
+// }
+// let ans = add(10,20);
+// console.log("The answer is ", ans);
+// console.log(add(10));
 
-let ans = add(10,20);
-console.log("The answer is ", ans);
+
+
+
+// -> 2. REST OPERATOR : taking multiple parameters in one variable 
+//  while the spread operator expands elements of an iterable, the rest operator compresses them.
+
+// function calculate(val1 , val2 , ...num)
+// {
+//     return  num[0] + num[1] + num[2] + num[3];
+// }
+// console.log(calculate(10,20,30,40));
+
+
+
+// -> 3. passing any object to a function
+// function tellName(obj)
+// {
+//     console.log(`The username is ${obj.name}`);
+// }
+// tellName({name:"Shrey", age:19})
+
+
+
+// -> 4. passing any array to a function
+// function tellSecondValue(arr)
+// {
+//     console.log(`The second value is ${arr[1]}`);
+// }
+// tellSecondValue([10,100,1000])
+
+
+
+// -> 5. The Optional Chaining Operator (?.)
+// The ?. operator returns undefined if an object is undefined or null (instead of throwing an error).
+// function tellName(obj)
+// {
+//     console.log(`The username is ${obj?.name}`);
+// }
+// tellName()
