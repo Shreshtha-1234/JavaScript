48452200c55dfbd8d06db1941a537019f60b8ad0
Shreshtha Gupta <101451400+Shreshtha-1234@users.noreply.github.com>
2023-08-07 17:12:08 +0000

    conditional

---
+++ 03_ExploringJS/04_controlFlow.js
@@ -0,0 +1,64 @@
+// Control Flow in JavaScript 
+
+let a = 100
+
+// if(a>100)
+// console.log("line1"),
+// console.log("line2");
+
+// if you dont use {} then implicitly it will take one line only but if you want it to multiple lines in if then use comma between them and end with semicolon
+
+
+
+// using switch case
+// let val = 5
+// switch(val)
+// {
+//     case 1:
+//         console.log(1);
+//         break;
+//     case 2:
+//         console.log(2);
+//         break;
+//     case 3:
+//         console.log(3);
+//         break;
+//     case 4:
+//         console.log(4);
+//         break;
+//     case 5:
+//         console.log(5);
+   
+//     case 6:
+//         console.log(6);
+        
+//     default:
+//         console.log(100);
+// }
+
+
+
+// falsyy value : false , 0 , -0, 0n(bigInt) ,empty string "" ,null , undefined , Nan
+
+// truthyy value : true , empty array , all +ve -ve ints , zero inside string , false in string , empty space in string , empty function fucntion(){}, empty object 
+
+
+// how to check empty string
+// if(string)
+
+// how to check empty array
+// if(Array.length === 0 )
+
+// how to check empty object
+// if(Object.keys(objectName).length == 0)
+
+
+
+
+// Null Coalescing operator (null  , undefined)
+
+// let x = 10 ?? 20
+// let y = null ?? 30
+// z = null ?? 10 ?? 20  first value 
+// console.log(x);
+// console.log(z);
